37f9be76f0e3b62f4fa8c071d3223dd675da9c0f
JosieEsth <josie_Esthaliani@yahoo.co.id>
2020-10-12 11:14:01 +0700

    Coba binding

--- app/src/main/java/com/pppb/tb01/MainActivity.kt
+++ app/src/main/java/com/pppb/tb01/MainActivity.kt
@@ -2,10 +2,16 @@ package com.pppb.tb01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pppb.tb01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(this.binding.root)
+
+
     }
 }
