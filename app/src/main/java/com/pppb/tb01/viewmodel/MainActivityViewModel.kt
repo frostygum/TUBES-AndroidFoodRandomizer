13f90f07bee507f137ef2dbd9178e4cd66129cc4
frostygum <4nthonius.juan5@gmail.com>
2020-10-15 09:10:39 +0700

    layout detail menu

--- app/src/main/java/com/pppb/tb01/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/pppb/tb01/viewmodel/MainActivityViewModel.kt
@@ -2,7 +2,7 @@ package com.pppb.tb01.viewmodel
 
 import androidx.lifecycle.ViewModel
 
-class MainActivityViewModel: ViewModel() {
+class MainActivityViewModel(): ViewModel() {
     init {
 
     }
